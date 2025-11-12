b60c29302a3f0cb0a29da18c6235fa0c33dd6f7f
Nadav-Zilberberg <nadav.z@rilloo.com>
2025-11-12 18:22:27 +0200

    Delete src/bitutils.c

--- src/bitutils.c
+++
@@ -1,5 +0,0 @@
-#include "bitutils.h"
-
-// This file now serves as a bridge to the C++ implementation
-// The actual implementation is in src_cpp/bitutils.cpp
-// The initialization is handled automatically by the C++ RAII pattern
